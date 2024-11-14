19ebbe889a5355a54af90f9327ed7afa0e50edb8
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2024-11-14 15:25:16 -0300

    Dockerfile deploy

---
+++ src/main/java/dev/cleyton/todolist/task/TaskRepository.java
@@ -0,0 +1,10 @@
+package dev.cleyton.todolist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+    public List<TaskModel> findByIdUser(UUID idUser);
+}
